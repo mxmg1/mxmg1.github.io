f1049b614dec219a6703c53fb65ce59ec0edb3fa
mxmg1 <themixmg1@gmail.com>
2024-01-27 16:13:10 +0100

    ok

--- scripts.js
+++ scripts.js
@@ -1,13 +1,3 @@
 import { Client } from "osu-web.js";
 
-const url = new URL("https://osu.ppy.sh/api/v2/oauth/tokens/current");
-
-const headers = {
-  "Content-Type": "application/json",
-  Accept: "application/json",
-};
-
-fetch(url, {
-  method: "DELETE",
-  headers,
-}).then((response) => response.json());
+const api = new Client(accessToken);
