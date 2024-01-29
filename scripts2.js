9ecd42e6b4aa274a52007edca1fad79a3df3493b
mxmg1 <themixmg1@gmail.com>
2024-01-30 00:28:41 +0100

    siema tu mix

--- scripts2.js
+++ scripts2.js
@@ -1,14 +1,13 @@
+function changeSlide(index) {
+  const slider = document.getElementById("slider");
+  slider.scrollTo({
+    left: index * slider.clientWidth,
+    behavior: "smooth",
+  });
 
-  function changeSlide(index) {
-    const slider = document.getElementById('slider');
-    slider.scrollTo({
-      left: index * slider.clientWidth,
-      behavior: 'smooth'
-    });
-
-    // Opcjonalnie: Zaznacz aktualny element nawigacyjny
-    const navElements = document.querySelectorAll('.slider-nav a');
-    navElements.forEach((nav, i) => {
-      nav.classList.toggle('active', i === index);
-    });
-  }
+  // Opcjonalnie: Zaznacz aktualny element nawigacyjny
+  const navElements = document.querySelectorAll(".slider-nav a");
+  navElements.forEach((nav, i) => {
+    nav.classList.toggle("active", i === index);
+  });
+}
